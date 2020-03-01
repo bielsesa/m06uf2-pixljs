4df618f8f56def6243c4aa9592df3c4730029e79
Biel Serrano Sanchez <bielsesa@gmail.com>
2020-03-01 21:40:05 +0100

    alsñkd

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
 const joc = {
+
 	getMenuItem = item => {
 		if (typeof item === 'string' && item == 'gameover')
 			return function () {
 				Pixl.menu(); // disable menu
+				item = 'Game Over';
 				g.clear();
 				g.setFontVector(20);
 				g.drawString(item, (g.getWidth() - g.stringWidth(item)) / 2, 10);
 				g.setFontBitmap();
 				g.drawString('Prem BTN3 per reiniciar', 15, 40);
 				g.flip();
-				setWatch(startGame, BTN3);
+				setWatch(comencaJoc, BTN3);
 			};
 		return function () {
-			showMenu(item);
+			mostraMenu(item);
 		};
 	},
-	showMenu = items => {
+	mostraMenu = items => {
 		const menu = {
 			'': { title: '-= TRIVIADOS =-' },
 		};
 		for (const i in items) menu[i] = getMenuItem(items[i]);
 		Pixl.menu(menu);
 	},
-	startGame = () => {
-		showMenu(tree);
+	comencaJoc = () => {
+		mostraMenu(tree);
+	},
+	mostraTitol = () => {
+		g.clear();
+		g.setFontBitmap();
+		g.drawString("Prem BTN3 per jugar");
+		g.setFontVector(40);
+		g.drawString('TRIVIA', (g.getWidth()-g.stringWidth('TRIVIA'))/2,10);
+		g.flip();
+		setWatch(comencaJoc, BTN3);
 	}
 };
+
+joc.mostraTitol();
--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const port = 3000;
 app.use(express.static('docs'));
 
 app.get('/pixljs/main', (req, res) => {
-    res.sendFile('./main.js');
+    res.writeHead(200, {
+        "Content-Type": "text/javascript; charset=utf-8"
+        });
+    
+    console.log(`Enviant main.js:\n${fs.readFile(`${__dirname}/main.js`)}`);
+    res.sendFile(`${__dirname}/main.js`);
+    res.end();
 });
 
 app.listen(port, () => console.log(`Escoltant pel port ${port}!`))
